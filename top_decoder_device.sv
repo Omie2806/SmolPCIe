6e451bfd407a4d84a1a24e8c8618c88d9ed9423c
Om <omgupta2806@gmail.com>
2026-08-01 23:36:30 +0530

    io and mem support

--- top_decoder_device.sv
+++ top_decoder_device.sv
@@ -15,6 +15,16 @@ logic [7 : 0] Bus_Number;
 logic [4 : 0] Device_ID;
 logic [2 : 0] Function_Number;
 logic [7 : 0] Register_Number;
+logic DW_3_MEM_READ;
+logic DW_3_MEM_WRITE;
+logic DW_4_MEM_READ;
+logic DW_4_MEM_WRITE;
+logic [31 : 0] Address_lower; //used for IO too
+logic [31 : 0] Address_upper; //used for MEM only 
+    //IO Requests
+logic IO_READ;
+logic IO_WRITE;
+logic debug_read;
 
 TLP_Decoder TLP_dut (
     .Header(Header),
@@ -23,7 +33,15 @@ TLP_Decoder TLP_dut (
     .Bus_Number(Bus_Number),
     .Device_ID(Device_ID),
     .Function_Number(Function_Number),
-    .Register_Number(Register_Number)
+    .Register_Number(Register_Number),
+    .DW_3_MEM_READ(DW_3_MEM_READ),
+    .DW_3_MEM_WRITE(DW_3_MEM_WRITE),
+    .DW_4_MEM_READ(DW_4_MEM_READ),
+    .DW_4_MEM_WRITE(DW_4_MEM_WRITE),
+    .Address_lower(Address_lower),
+    .Address_upper(Address_upper),
+    .IO_READ(IO_READ),
+    .IO_WRITE(IO_WRITE)
 );
 
 device_1 device_dut (
@@ -33,8 +51,16 @@ device_1 device_dut (
     .CONFIG_WRITE(CONFIG_WRITE),
     .Device_ID_Decoder(Device_ID),
     .Register_Number(Register_Number),
-    .Function_Number(Function_Number),
     .data_in(data_in),
-    .data_out(data_out)
+    .data_out(data_out),
+    .DW_3_MEM_READ(DW_3_MEM_READ),
+    .DW_3_MEM_WRITE(DW_3_MEM_WRITE),
+    .DW_4_MEM_READ(DW_4_MEM_READ),
+    .DW_4_MEM_WRITE(DW_4_MEM_WRITE),
+    .Address_lower(Address_lower),
+    .Address_upper(Address_upper),
+    .IO_READ(IO_READ),
+    .IO_WRITE(IO_WRITE),
+    .debug_read(debug_read)
 );
 endmodule
