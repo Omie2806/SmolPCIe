9ee877df2b3d989f9ae4cea4f4da910769eb3fe6
Om <omgupta2806@gmail.com>
2026-08-02 20:37:47 +0530

    compl module

--- top_decoder_device.sv
+++ top_decoder_device.sv
@@ -5,8 +5,12 @@ module top_decoder_device #(
     input logic clk,
     input logic reset,
     input logic [DATA_WIDTH - 1 : 0] Header [0 : TOTAL_DW - 1],
-    input logic[DATA_WIDTH - 1 : 0] data_in,
-    output logic[DATA_WIDTH - 1 : 0] data_out
+    input logic [DATA_WIDTH - 1 : 0] data_in,
+    output logic[DATA_WIDTH - 1 : 0] Completion_Header[0 : TOTAL_DW - 1],
+    output logic[DATA_WIDTH - 1 : 0] data_out,
+    output logic Compl_Read,
+    output logic Compl_Write,
+    output logic mem_write_done
 );
 
 logic CONFIG_READ;
@@ -24,7 +28,21 @@ logic [31 : 0] Address_upper; //used for MEM only
     //IO Requests
 logic IO_READ;
 logic IO_WRITE;
-logic debug_read;
+logic [2 : 0] Completer_Function;
+logic [4 : 0] Completer_Dev;
+logic [2 : 0] Compl_status;//completion always completes for now
+logic [11 : 0] Byte_count; //number of bytes remaining to complete a read request with multiple completions
+
+logic [2 : 0] TC;
+logic [2 : 0] Attr;
+logic [9 : 0] Length; //to be used for MEM AND IO too
+logic [3 : 0] F_DW_BYTE_EN;
+logic [3 : 0] L_DW_BYTE_EN; //lower address generation
+logic [15 : 0] Requester_ID;
+logic [7 : 0] Tag;
+
+logic[7 : 0] Completer_Bus;
+assign Completer_Bus = 8'b0; //for single system
 
 TLP_Decoder TLP_dut (
     .Header(Header),
@@ -41,7 +59,14 @@ TLP_Decoder TLP_dut (
     .Address_lower(Address_lower),
     .Address_upper(Address_upper),
     .IO_READ(IO_READ),
-    .IO_WRITE(IO_WRITE)
+    .IO_WRITE(IO_WRITE),
+    .TC(TC),
+    .Attr(Attr),
+    .Length(Length),
+    .F_DW_BYTE_EN(F_DW_BYTE_EN),
+    .L_DW_BYTE_EN(L_DW_BYTE_EN),
+    .Requester_ID(Requester_ID),
+    .Tag(Tag)
 );
 
 device_1 device_dut (
@@ -61,6 +86,32 @@ device_1 device_dut (
     .Address_upper(Address_upper),
     .IO_READ(IO_READ),
     .IO_WRITE(IO_WRITE),
-    .debug_read(debug_read)
+    .Compl_Read(Compl_Read),
+    .Compl_Write(Compl_Write),
+    .mem_write_done(mem_write_done),
+    .Completer_Function(Completer_Function),
+    .Completer_Dev(Completer_Dev),
+    .Compl_status(Compl_status),
+    .Byte_count(Byte_count),
+    .Length(Length)
+);
+
+completion_gen completion_dut (
+    .Compl_Read(Compl_Read),
+    .Compl_Write(Compl_Write),
+    .TC(TC),
+    .Attr(Attr),
+    .Length(Length),
+    .Byte_count(Byte_count),
+    .F_DW_BYTE_EN(F_DW_BYTE_EN),
+    .L_DW_BYTE_EN(L_DW_BYTE_EN),
+    .Requester_ID(Requester_ID),
+    .Tag(Tag),
+    .Completer_Bus(Completer_Bus),
+    .Completer_Dev(Completer_Dev),
+    .Completer_Function(Completer_Function),
+    .Compl_status(Compl_status),
+    .Address_lower(Address_lower),
+    .Completion_Header(Completion_Header)
 );
 endmodule
