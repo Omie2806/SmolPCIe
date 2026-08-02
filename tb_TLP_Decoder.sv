378afc3696e6e5583f31d719e22cd58cf4d0940c
Om <omgupta2806@gmail.com>
2026-08-02 20:37:22 +0530

    compl tests

--- tb_TLP_Decoder.sv
+++ tb_TLP_Decoder.sv
@@ -7,7 +7,11 @@ parameter TOTAL_DW        = 4;
     logic reset;
     logic [DATA_WIDTH - 1 : 0] Header [0 : TOTAL_DW - 1]; //receiving the header 
     logic [DATA_WIDTH - 1 : 0] data_in;
+    logic [DATA_WIDTH - 1 : 0] Completion_Header [0 : TOTAL_DW - 1];
     logic [DATA_WIDTH - 1 : 0] data_out;
+    logic Compl_Read;
+    logic Compl_Write;
+    logic mem_write_done;
 
 top_decoder_device dut (.*);
 
@@ -41,7 +45,7 @@ initial begin
     Header[0] = 32'b010_00100_0_000_0_0_0_0_1_1_00_00_0000000001; //config write on BAR 0 address
     Header[1] = 32'h01_00_00_0_f;
     Header[2] = 32'b0000_0001_0000_0000_0000_0000_0001_0000;
-    data_in   = 32'hF900_0000;
+    data_in   = 32'h0000_0001;
     repeat(2)@(posedge clk);
 
     Header[0] = 32'b000_00100_0_000_0_0_0_0_1_1_00_00_0000000001; //config read on BAR 0 read the written address
@@ -133,19 +137,37 @@ initial begin
 //ive already verified
 
     //sent a memory request
-    Header[0] = 32'b011_00000_0_000_0_0_0_0_1_1_00_00_0000000001; 
+    Header[0] = 32'b010_00000_0_000_0_0_0_0_1_1_00_00_0000000010; //mem write 3 dw on 000f onwards
     Header[1] = 32'h01_00_00_f_f;
-    Header[2] = 32'h0000_0002;
-    Header[3] = 32'h4000_4002;
-    data_in   = 32'h0000_4000;
-    repeat(2)@(posedge clk);
+    Header[2] = 32'h0000_000F;
+    Header[3] = 32'h0;
+    @(posedge clk);
+    data_in   = 32'h0000_4002;
+    @(posedge clk); //it doesn't matter if multiple writes happen to the same address with the same data
+    data_in   = 32'h0000_4001;
+    do
+        @(posedge clk);
+    while (!mem_write_done);
+
+    //sent a memory request
+    Header[0] = 32'b000_00000_0_000_0_0_0_0_1_1_00_00_0000000010; //mem read 3 dw on 000f onwards
+    Header[1] = 32'h01_00_00_f_f;
+    Header[2] = 32'h0000_000F;
+    Header[3] = 32'h0;
+    do
+        @(posedge clk);
+    while (!(Compl_Read || Compl_Write));
+
 
     Header[0] = 32'b010_00010_0_000_0_0_0_0_1_1_00_00_0000000001; //io write request
     Header[1] = 32'h01_00_00_f_f;
     Header[2] = 32'h0000_40F0;
     Header[3] = 32'h0;
     data_in   = 32'h0000_400F;
-    repeat(2)@(posedge clk);
+    do
+        @(posedge clk);
+    while (!(Compl_Read || Compl_Write));
+
 
     Header[0] = 32'b000_00010_0_000_0_0_0_0_1_1_00_00_0000000001; //io read request shouldnt take place
     Header[1] = 32'h01_00_00_f_f;
@@ -157,6 +179,10 @@ initial begin
     Header[1] = 32'h01_00_00_f_f;
     Header[2] = 32'h0000_40F0;
     Header[3] = 32'h0;
+    do
+        @(posedge clk);
+    while (!(Compl_Read || Compl_Write));
+
     repeat(2)@(posedge clk);
 
     $finish; 
