d793ea1491128e542f3f37bd767570c2cb1a9163
Om <omgupta2806@gmail.com>
2026-08-15 12:10:42 +0530

    multi dw support and TLP outputs

--- main src/top_Decoder_Encoder.sv
+++ main src/top_Decoder_Encoder.sv
@@ -24,13 +24,18 @@ module top_Decoder_Encoder #(
     input logic [2 : 0] Attr,
     input logic [3 : 0] F_DW_BYTE_EN,
     input logic [3 : 0] L_DW_BYTE_EN,
-    input logic [DATA_WIDTH - 1 : 0] data_in,
-    output logic [DATA_WIDTH - 1 : 0] data_out,
-    output logic [15 : 0] Requester_ID_Compl
+    input logic [DATA_WIDTH - 1 : 0] data_in [0 : 1],
+    output logic [DATA_WIDTH - 1 : 0] data_out [0 : 1],
+    output logic [15 : 0] Requester_ID_Compl,
+    output logic [DATA_WIDTH - 1 : 0] TLP_IN [0 : 5],
+    output logic [DATA_WIDTH - 1 : 0] TLP_OUT [0 : 5]
 );
 
-logic [DATA_WIDTH - 1 : 0] Completion_Header[0 : TOTAL_DW - 1];
+logic [DATA_WIDTH - 1 : 0] Completion_Header [0 : TOTAL_DW - 1];
+logic [DATA_WIDTH - 1 : 0] Completion_Header_decoder [0 : TOTAL_DW - 1];
 logic [DATA_WIDTH - 1 : 0] Header [0 : TOTAL_DW - 1];
+logic [DATA_WIDTH - 1 : 0] data_in_device [0 : 1];
+
 logic Compl_Read;
 logic Compl_Write;
 logic mem_write_done;
@@ -68,11 +73,35 @@ logic [7 : 0] Tag;
         .Header(Header)
     );
 
+    always_comb begin
+        TLP_IN[0] = Header[0];
+        TLP_IN[1] = Header[1];
+        TLP_IN[2] = Header[2];
+        TLP_IN[3] = Header[3];
+        TLP_IN[4] = data_in[0];
+        TLP_IN[5] = data_in[1];
+
+        data_in_device[0] = TLP_IN[4];
+        data_in_device[1] = TLP_IN[5];
+
+        TLP_OUT[0] = Completion_Header[0];
+        TLP_OUT[1] = Completion_Header[1];
+        TLP_OUT[2] = Completion_Header[2];
+        TLP_OUT[3] = Completion_Header[3];
+        TLP_OUT[4] = data_out[0];
+        TLP_OUT[5] = data_out[1];
+
+        Completion_Header_decoder[0] = TLP_OUT[0];
+        Completion_Header_decoder[1] = TLP_OUT[1];
+        Completion_Header_decoder[2] = TLP_OUT[2];
+        Completion_Header_decoder[3] = TLP_OUT[3];
+    end
+
     top_decoder_device decoder_device_dut (
         .clk(clk),
         .reset(reset),
         .Header(Header),
-        .data_in(data_in),
+        .data_in(data_in_device),
         .Completion_Header(Completion_Header),
         .data_out(data_out),
         .Compl_Read(Compl_Read),
@@ -81,7 +110,7 @@ logic [7 : 0] Tag;
     );
 
     Completer_Decoder completion_decoder_dut (
-        .Completion_Header(Completion_Header),
+        .Completion_Header(Completion_Header_decoder),
         .Completer_ID(Completer_ID),
         .Requester_ID(Requester_ID_Compl),
         .Compl_status(Compl_status),
